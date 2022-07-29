72c3d5eec1096ffd9aa0583c88e4db218bdf055a
김승녕 <seungnyeong@wemakeprice.com>
2022-07-29 09:56:56 +0900

    nmap and telnet check

--- go.mod
+++ go.mod
@@ -3,6 +3,9 @@ module wenmap
 go 1.17
 
 require (
+	github.com/reiver/go-oi v1.0.0 // indirect
+	github.com/reiver/go-telnet v0.0.0-20180421082511-9ff0b2ab096e // indirect
 	github.com/t94j0/array v0.0.0-20180426153242-68930562a6bd // indirect
 	github.com/t94j0/nmap v0.0.0-20211119144019-7081d966c08f // indirect
+	golang.org/x/text v0.3.7 // indirect
 )
--- main.go
+++ main.go
@@ -4,34 +4,31 @@ import (
 	"bufio"
 	"encoding/csv"
 	"fmt"
-	"log"
-	"os"
-	"strings"
-
 	"github.com/t94j0/nmap"
+	"log"
+	"net"
+	"os"
+	"regexp"
+	"strings"
+	"time"
 )
 
-
 type target struct {
-	hostname 	string
-	types	 	string
-	publicIp 	string
-	privateIp 	string
-	zone		string
-	category 	string
-	position	string
-	status		string
-	kind		string
-	osType		string
-	result		string
+	hostname string
+	domain   string
+	result   string
+	telnet   string
 }
 
+type caller struct{}
+
 func main() {
-	
+
 	file, _ := os.Open("./target.csv")
 	rdr := csv.NewReader(bufio.NewReader(file))
 	rows, error := rdr.ReadAll()
 	checkErr(error)
+	r := regexp.MustCompile("Port [0-9]+")
 
 	var targets []target
 	var results []target
@@ -44,23 +41,51 @@ func main() {
 	}
 
 	mainC := make(chan target)
-	
 
 	for _, target := range targets {
 		go scanTarget(target, mainC)
 	}
-	
-	for i:=0; i < len(targets); i++{
+
+	for i := 0; i < len(targets); i++ {
 		targets := <-mainC
+		a := r.FindAllString(targets.result, -1)
+		b := telnetCheck(a, targets.domain)
+		targets.telnet = b
+
 		results = append(results, targets)
-		
+
 	}
-	
+
 	writeCSV(results)
 	fmt.Println("Finished Scan")
 }
 
+func telnetCheck(ports []string, domain string) string {
+	re := regexp.MustCompile("[0-9]+")
+	result := ""
+	timeout := time.Second
+
+	for i := 0; i < len(ports); i++ {
+		t := re.FindAllString(ports[i], -1)
+		s := fmt.Sprintf("%s:%s", domain, t[0])
+
+		conn, err := net.DialTimeout("tcp", s, timeout)
+
+		fmt.Println(s)
+		if err != nil {
+			fmt.Println("Connecting error:", err)
+		}
+		if conn != nil {
+			defer conn.Close()
+			result += "\n" + fmt.Sprintf("Opend %s ", s)
+		}
+
+	}
+	return result
+}
+
 func writeCSV(sResult []target) {
+
 	fmt.Println("File Write")
 	file, err := os.Create("result2.csv")
 	checkErr(err)
@@ -68,75 +93,47 @@ func writeCSV(sResult []target) {
 	w := csv.NewWriter(file)
 
 	headers := []string{
-		"hostName", 
-		"purpose", 
-		"publicIP", 
-		"privateIP", 
-		"zonePosition", 
-		"category", 
-		"assetPosition", 
-		"status", 
-		"subCategory", 
-		"osType",
+		"hostname",
+		"domain",
 		"result",
+		"telnet",
 	}
 
 	wErr := w.Write(headers)
+
 	defer w.Flush()
 	checkErr(wErr)
 	for _, result := range sResult {
 		resultSlice := []string{
-				result.hostname, 
-				result.types, 
-				result.publicIp, 
-				result.privateIp, 
-				result.zone, 
-				result.kind, 
-				result.position, 
-				result.status, 
-				result.kind, 
-				result.osType, 
-				result.result,
+			result.hostname,
+			result.domain,
+			result.result,
+			result.telnet,
 		}
 		resultErr := w.Write(resultSlice)
+
 		checkErr(resultErr)
 	}
 }
 
 func scanTarget(item target, c chan<- target) {
 	scan, _ := nmap.Init().
-				AddHosts(item.publicIp).
-				AddPortRange(0, 1000).
-				AddFlags("-sS").Run()
-	
-	c <-  target {
-			hostname : item.hostname ,
-			types	 : item.types	 ,
-			publicIp : item.publicIp ,
-			privateIp : item.privateIp,
-			zone	 : item.zone		,	
-			category : item.category ,
-			position : item.position	,	
-			status	 : item.status		,	
-			kind	 : item.kind		,	
-			osType	 : item.osType		,	
-			result	 : CleanString(scan.ToString())	,	
+		AddHosts(item.domain).
+		AddPorts(21, 22, 23, 25, 53, 80, 389, 443, 445, 3306, 5432, 1521, 2638, 1433, 3389, 8080, 9000, 9200, 8443, 5601, 5000, 3000, 27017).
+		AddFlags("--open").
+		AddFlags("-sT").Run()
+	c <- target{
+		hostname: item.hostname,
+		domain:   item.domain,
+		result:   scan.ToString(),
 	}
 }
 
 func extractTarget(row []string, c chan<- target) {
 
-	c <- target {
-		hostname : row[0],
-		types : row[1],
-		publicIp : row[2],
-		privateIp : row[3],
-		zone : row[4],
-		category : row[5],
-		position : row[6],
-		status : row[7],
-		kind : row[8],
-		osType : row[9],
+	c <- target{
+		hostname: row[0],
+		domain:   row[1],
 	}
 }
 
@@ -147,5 +144,9 @@ func checkErr(err error) {
 }
 
 func CleanString(str string) string {
-	return strings.Join(strings.Fields(strings.TrimSpace(str)), " ")
+	if strings.ContainsAny(str, "open") {
+		fmt.Println(str)
+		return strings.Join(strings.Fields(strings.TrimSpace(str)), " ")
+	}
+	return ""
 }
